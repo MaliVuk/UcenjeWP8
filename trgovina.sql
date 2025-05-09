e8099df6dec8cf969ad2b262364ccc9f17e232d0
MaliVuk <zltvvu7@gmail.com>
2025-05-10 00:53:21 +0200

    frizerski salon

--- trgovina.sql
+++ trgovina.sql
@@ -1,23 +1,23 @@
-﻿--create database trgovina;
---use trgovina;
+﻿create database trgovina;
+use trgovina;
 
---create table racuni(
+create table racuni(
 
---sifra int,
+sifra int,
 
---broj varchar(10),
---kupac varchar(100)
---);
+broj varchar(10),
+kupac varchar(100)
+);
 
---create table proizvodi(
+create table proizvodi(
 
---sifra int,
---naziv varchar(50),
---cijena decimal(18,2)
---);
+sifra int,
+naziv varchar(50),
+cijena decimal(18,2)
+);
 
---create table stavke(
---racun int,
---proizvod int,
---kolicina decimal(5,3)
---);
+create table stavke(
+racun int,
+proizvod int,
+kolicina decimal(5,3)
+);
