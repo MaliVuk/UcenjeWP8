53096926598be59ffd7d35994502e15f951e6687
zlata <zltvvu7@gmail.com>
2025-05-09 18:31:29 +0200

    kreiranje tablice

--- skriptawp8.sql
+++ skriptawp8.sql
@@ -6,12 +6,21 @@
 --use edunovawp8;
 --drop database edunovawp8
 
-create table smjerovi(
+--create table smjerovi(
 
-sifra int,
-naziv varchar(50),
-cijena decimal(18,2),
-datumpokretanja datetime,
-aktivan bit
-);
+--sifra int,
+--naziv varchar(50),
+--cijena decimal(18,2),
+--datumpokretanja datetime,
+--aktivan bit
+--);
 
+
+
+--create table grupe(
+--sifra int,
+--naziv varchar(20),
+--smjer int,
+--predavac varchar(50)
+
+--);
