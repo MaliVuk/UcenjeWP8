f2e806e90e5b8b6a7a46cbf4c92f5b302b3024b7
MaliVuk <zltvvu7@gmail.com>
2025-05-12 00:32:25 +0200

    Update muzej.sql

--- muzej.sql
+++ muzej.sql
@@ -7,11 +7,40 @@
 --);
 
 create table sponzori (
+﻿create database muzej;
+go
+use muzej;
+go
+create table kustosi (
 sifra int primary key,
 ime_i_prezime varchar(100),
 mail varchar (100)
-);naziv varchar(50),
+);
+go
+create table sponzori (
+sifra int primary key,
 naziv varchar(50),
 iznos_uplate decimal (10,2)
 );
+go
+create table izlozbe (
+sifra int primary key,
+naziv_izlozbe varchar (50),
+vrsta_izlozbe varchar (50),
+sifra_kustosa int unique,
+sifra_sponzora int unique, 
+foreign key (sifra_kustosa) references kustosi(sifra),
+foreign key (sifra_sponzora) references sponzori(sifra)
+);
+go
+CREATE TABLE Djela (
+    sifra INT PRIMARY KEY,
+    naziv_djela VARCHAR(100),
+    vrsta_djela VARCHAR(100),
+    cijena DECIMAL(10,2),
+    sifra_izlozbe INT,
+    FOREIGN KEY (sifra_izlozbe) REFERENCES izlozbe(sifra)
+);
+
+
 
