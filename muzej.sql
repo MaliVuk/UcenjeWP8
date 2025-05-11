a50e76efd50d9cedc584cf2b00f59ae1b39b7b7e
MaliVuk <zltvvu7@gmail.com>
2025-05-11 20:02:14 +0200

    Update muzej.sql

--- muzej.sql
+++ muzej.sql
@@ -1,7 +1,16 @@
 ﻿--create database muzej;
 --use muzej;
-create table kustosi (
+--create table kustosi (
+--sifra int primary key,
+--ime_i_prezime varchar(100),
+--mail varchar (100)
+--);
+
+create table sponzori (
 sifra int primary key,
 ime_i_prezime varchar(100),
 mail varchar (100)
+);naziv varchar(50),
+iznos_uplate decimal (10,2)
 );
+
