b896ecff8d32b22fded2ce9cd7d9d58052073576
MaliVuk <zltvvu7@gmail.com>
2025-05-12 16:23:35 +0200

    Create azilzazivotinje2.sql

---
+++ azilzazivotinje2.sql
@@ -0,0 +1,23 @@
+﻿--create database azil_za_zivotinje;
+--use azil_za_zivotinje;
+
+--create table djelatnici(
+--sifra int
+--ime_i_prezime varchar (50),
+--zanimanje varchar (50)
+--);
+--create table zivotinje (
+--Sifra int,
+--ime varchar (20),
+--dob int
+--);
+
+create table prostorije (
+sifra int,
+boja_prostorije varchar (20)
+);
+
+create table boravak (
+
+
+);
