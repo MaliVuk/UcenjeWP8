c087c28adc0b269ef79abc63ff68aede3356b24b
MaliVuk <zltvvu7@gmail.com>
2025-05-13 21:37:26 +0200

    izmjene

--- frizierskisalon.sql
+++ frizierskisalon.sql
@@ -1,36 +1,34 @@
-﻿CREATE DATABASE frizerski_salon;   
-use master;
+﻿use master;
 go
-drop database if exits frizerski_salon;
+drop database if exists frizerski_salon;
 go
-create database frizerski_salon
+create database frizerski_salon;
+go
 use frizerski_salon;
 
 create table djelatnica (
-sifra int not null identity(1,1) primary key,
+sifra int not null primary key identity(1,1),
 ime varchar (100)not null,
 prezime varchar (100)not null
 );
+
 create table korisnik (
 sifra int not null primary key identity(1,1),
-spol varchar  (10) not null,
+spol varchar  (10) ,
 br_telefona varchar (20) not null
 );
 
 create table usluga (
-sifra int not null primary key,
-naziv  not null varchar(50),
-cijena decimal(18,2) not nul
+sifra int not null primary key identity (1,1),
+naziv varchar(50)  not null ,
+cijena decimal(18,2) not null
 );
 
 create table posjeta (
-sifra int not null identity(1,1)primary key,
-djelatnica not null int,
-korisnik not null int,
-usluga  not nul int,
-    FOREIGN KEY (djelatnica) not nul REFERENCES Djelatnica(sifra),
-    FOREIGN KEY (korisnik) REFERENCES Korisnik(sifra),
-    FOREIGN KEY (usluga) REFERENCES Usluga(sifra)
-
+sifra int not null primary key identity(1,1),
+djelatnica  int not null references djelatnica,
+korisnik  int not null references korisnik,
+usluga  int not null references usluga
+   
 );
 
