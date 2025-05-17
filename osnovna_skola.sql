b50127779f0b59dc0fbd221346a8e6245c2c3049
MaliVuk <zltvvu7@gmail.com>
2025-05-17 18:02:11 +0200

    osnovna skola

---
+++ osnovna_skola.sql
@@ -0,0 +1,31 @@
+﻿
+create database osnovna_skola;
+go
+use osnovna_skola;
+go
+
+create table uciteljica (
+sifra int  not null primary key identity (1,1),
+ime varchar (50),
+prezime varchar (100),
+predmet varchar (100)
+);
+
+create table radionica (
+sifra int not null primary key identity (1,1),
+naziv varchar (100),
+uciteljica int not null references uciteljica
+);
+
+create table dijete (
+sifra  int not null primary key identity (1,1),
+ime varchar (50) not null,
+prezime varchar (100)not null,
+razred varchar (10)
+);
+
+create table sudjelovanje (
+sifra int not null primary key identity (1,1),
+dijete int not null references dijete,
+radionica int not null references radionica
+);
