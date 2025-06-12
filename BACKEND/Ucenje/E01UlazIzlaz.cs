1737c7afdcabc9e6c524d1e1f53e57b1b454147d
MaliVuk <zltvvu7@gmail.com>
2025-06-12 21:54:11 +0200

    backend

---
+++ BACKEND/Ucenje/E01UlazIzlaz.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Ucenje
+{
+    internal class E01UlazIzlaz
+    {
+
+        // unutar klase radimo metodu
+        public static void Izvedi()
+        {
+
+
+            Console.Write("Pozdrav Osijeku");
+
+            Console.WriteLine("Isti red");
+
+            Console.WriteLine("Osijek je \n SUPER"); // \n je novi red
+
+            Console.WriteLine("\t\tOvo je uvučeno");
+
+            Console.Write("Unesi svoje ime: ");
+
+            // varijabla ime
+            string ime = Console.ReadLine();
+
+
+            Console.WriteLine("Unijeli ste {0}", ime);
+
+        } // kraj metode
+
+
+
+
+
+    } // kraj klase
+} // kraj imenskog prostora (namespace)
