fafddaeb038d1fb0ee396f2e7bef43de22086cc4
MaliVuk <zltvvu7@gmail.com>
2025-06-18 19:38:29 +0200

    novo

---
+++ BACKEND/Ucenje/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+using Ucenje;
+
+
+Ucenje.E14KlasaObjekt.Program.AutomobilZadatak();
+
+
+
+// ključna riječ new poziva konstruktor klase
+//new Ucenje.E14KlasaObjekt.Program();
+
+//E11Z02.Izvedi();
+
+
+//E13Rekurzija.Izvedi();
+
+//E12Metode.Izvedi();
+
+//E11Z01.Izvedi();
+
+//E11TryCatch.Izvedi();
+
+//E10ForEach.Izvedi();
+
+//E09DoWhile.Izvedi();
+
+//E08WhilePetlja.Izvedi();
+
+//E07ForPetlja.Izvedi();
+
+
+//E06Zad1.Izvedi();
+
+
+//06Nizovi.Izvedi();
+
+//E05UvjetnoGrananjeSwitch.Izvedi();
+
+//E04UvjetnoGrananjeIf.Izvedi();
+//E03Operatori.Izvedi();
+//E02Varijable.Izvedi();
+//E01UlazIzlaz.Izvedi();
+
+
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
